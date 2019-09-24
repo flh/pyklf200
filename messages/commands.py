16e6165a71c975c9cd90a4f17fc4aec5688e6c3c
Florian Hatat <github@hatat.me>
2019-09-24 17:52:37 +0200

    Explicitely license all code under AGPL3

--- messages/commands.py
+++ messages/commands.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# pyKlf200 - Python client implementation of the Velux KLF200 protocol
+# Copyright (c) 2019 Florian Hatat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 List of commands understood or sent by the KLF-200 gateway
 """
