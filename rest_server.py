24c76202696faf482672623ade1b9697f916d48f
Florian Hatat <github@hatat.me>
2019-09-28 17:51:17 +0200

    Fix access to utc_time attribute

--- rest_server.py
+++ rest_server.py
@@ -200,7 +200,7 @@ class RestClientConnection(asyncio.Protocol):
     async def GET_clock(self, request):
         clock_info = await self.klf_client.send(messages.general.GetLocalTimeReq())
         await self.write_simple_response(body={
-            'time': clock_info.isoformat(),
+            'time': clock_info.utc_time.isoformat(),
         })
 
     async def POST_clock(self, request):
